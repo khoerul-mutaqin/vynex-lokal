e9f9065076439055ac67fb62d89f9346b81257b4
khoerul-mutaqin <khoerul.mutaqin@falinwa.com>
2026-06-30 12:00:44 +0700

    [fix] not auto validate and not fill wrong demand

--- cdp_vendor_intracompany/models/stock.py
+++ cdp_vendor_intracompany/models/stock.py
@@ -1,4 +1,5 @@
 from odoo import models
+from odoo.exceptions import UserError
 
 
 class StockMove(models.Model):
@@ -6,6 +7,23 @@ class StockMove(models.Model):
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
+
+    def _cdp_update_move_qty(self):
+        for rec in self:
+            all_do = rec.filtered(
+                lambda x: x.state not in ["done", "cancel"]
+                and x.picking_type_id.code == "incoming"
+            )
+            stock_move_ids = all_do.group_id.stock_move_ids 
+            for current_move in all_do.move_ids_without_package:
+                for move in stock_move_ids:
+                    raise UserError(f"product {move.product_id} - curr demand :{current_move.product_uom_qty}  curr qty :{current_move.quantity} => state: {move.state} demand: {move.product_uom_qty} qty: {move.quantity}")                    
+                    if current_move.product_id == move.product_id and current_move.quantity > move.product_uom_qty and move.state != 'done':
+                        move.write({
+                            "product_uom_qty": current_move.quantity,
+                        })
+                        for ml in move.move_line_ids:
+                            ml.quantity = current_move.quantity                        
 
 
     def button_validate(self):
@@ -35,16 +53,16 @@ class StockPicking(models.Model):
                     lambda x: x.state not in ["done", "cancel"]
                 )
                 all_moves = all_do.mapped("move_ids_without_package")
-                for current_move in self.move_ids_without_package:
-                    for move in all_moves:
-                        if current_move.product_id == move.product_id:
-                            fields_sync.fal_run_sync_record(
-                                "stock.move", "stock.move", current_move, move
-                            )
+                # for current_move in self.move_ids_without_package:
+                #     for move in all_moves:
+                #         if current_move.product_id == move.product_id:
+                #             fields_sync.fal_run_sync_record(
+                #                 "stock.move", "stock.move", current_move, move
+                #             )
 
                 for do in all_do:
-                    if picking.state == "done":
-                        do.with_context(skip_sanity_check=True).button_validate()
+                    # if picking.state == "done":
+                        # do.with_context(skip_sanity_check=True).button_validate()
 
                     moves = (
                         do.mapped("backorder_ids")
